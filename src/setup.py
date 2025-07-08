175dd0fff407494d3eba04050edab32facf8ce7a
Daniel Alves <danielvesantana@gmail.com>
2025-07-07 21:09:01 -0300

    setup.py

--- src/setup.py
+++ src/setup.py
@@ -27,40 +27,39 @@ T = np.zeros(shape = (P,M), dtype = np.int32)      #Custo (em reais por tonelada
 
 D = csv_to_array(D, "../DATASET/custo_de_produção.csv")
 
-print(D)
+print("#PARAMETROS")
 
-# print("#PARAMETROS")
+#Variáveis
+solver = pywraplp.Solver('simple_lp_program', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+d_amp = {} #quantidade (em kg) comprada de alimento a ∈ A pelo unicípio m ∈ M do município p ∈ P.
+for a in range(A):
+    for m in range(M):
+        for p in range(P):
+            if(D[a][p] > 0): 
+                d_amp[(a,m,p)] = solver.NumVar(0,D[a][p]) 
 
-# #Variáveis
-# solver = pywraplp.Solver('simple_lp_program', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
-# d_amp = {} #quantidade (em kg) comprada de alimento a ∈ A pelo unicípio m ∈ M do município p ∈ P.
-# for a in range(A):
-#     for m in range(M):
-#         for p in range(P):
-#             if(D[a][p] > 0): 
-#                 d_amp[(a,m,p)] = solver.NumVar(0,D[a][p]) 
+'''
+#Restrições
+for a in range(A):
+    ct = [[solver.Constraint(D[a][p], D[a][p]) for a in range(A)] for p in range(P)]
 
-# #Restrições
-# for a in range(A):
-#     ct = [[solver.Constraint(D[a][p], D[a][p]) for a in range(A)] for p in range(P)]
+    for m in range(M):
+        ct.SetCoefficient(d_amp[a][m], 1)
 
-#     for m in range(M):
-#         ct.SetCoefficient(d_amp[a][m], 1)
+for a in range(A):
+    ct = [[solver.Constraint(Q[m]*R[n]) for m in range(M)] for n in range(N)]
 
-# for a in range(A):
-#     ct = [[solver.Constraint(Q[m]*R[n]) for m in range(M)] for n in range(N)]
+    #for m in range(M):
+        #ct.SetCoefficient(d_amp[a][m], [O[n][a] for n in range(N)] for a in range(A))
+'''
 
-#     #for m in range(M):
-#         #ct.SetCoefficient(d_amp[a][m], [O[n][a] for n in range(N)] for a in range(A))
+#Alimento disponível
 
 
-# #Alimento disponível
-
-
-# #Função Objetivo
-# objective = solver.Objective()
-# for amp in d_amp:
-#     objective.setCoefficient(d_amp[amp],C[a][p]+T[p][m])
+#Função Objetivo
+objective = solver.Objective()
+for amp in d_amp:
+    objective.setCoefficient(d_amp[amp],C[a][p]+T[p][m])
     
-# objective.SetMinimization()
-# #Resolução do problema
+objective.SetMinimization()
+#Resolução do problema
